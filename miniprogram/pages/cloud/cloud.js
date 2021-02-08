b0975d49b305340e804a724177d37ed663b493d8
齐磊 <qiilee@126.com>
2021-02-08 13:17:19 +0800

    add: 云函数

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -26,7 +26,7 @@ Page({
       console.log(err)
     })
   },
-  update: function(){
+  update: function () {
     db.collection('user').doc('79550af2601d0c6702bb0e0e1ffad24a').update({
       data: {
         name: 'qilei',
@@ -38,10 +38,48 @@ Page({
       console.log(err)
     })
   },
-  search: function(){
+  search: function () {
     db.collection('user').where({
       name: "qilei"
     }).get().then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  delete: function () {
+    db.collection('user').doc('79550af2601d0c6702bb0e0e1ffad24a').remove().then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  sum: function () {
+    wx.cloud.callFunction({
+      name: 'sum',
+      data: {
+        a: 2,
+        b: 3
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  getOpenId: function () {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  batchDelete: function () {
+    wx.cloud.callFunction({
+      name: 'batchDelete'
+    }).then(res => {
       console.log(res)
     }).catch(err => {
       console.log(err)
